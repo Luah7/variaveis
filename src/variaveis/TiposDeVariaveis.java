366bbe507e143a64956fad3eaafccd7a2bee7f13
luah7 <luanmachadoo365@gmail.com.br>
2023-02-17 21:35:50 -0300

    Primeiro comit

---
+++ src/variaveis/TiposDeVariaveis.java
@@ -0,0 +1,19 @@
+package variaveis;
+
+public class TiposDeVariaveis {
+	
+	public static void main(String[] args) {
+		int idade;
+		int livros;
+		int cadernos;
+		int canetas;
+		
+		idade = 18;
+		livros = 30;
+		cadernos = 2;
+		canetas = 4;
+		
+		System.out.println(idade * livros);
+	}
+
+}
